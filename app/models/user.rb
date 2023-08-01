f32a2cb42741ffed0695fc822563662e4fb6c9e0
Oscar <omankiewicz77@gmail.com>
2023-08-01 11:26:14 -0400

    added code to allow validations to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :password, length: { minimum: 6 }, if: :password_required?
     has_secure_password
+  
+    private
+  
+    def password_required?
+      new_record? || password.present?
+    end
+  
+    def self.authenticate_with_credentials(email, password)
+        user = self.find_by('lower(email) = ?', email.strip.downcase)
+        return nil unless user&.authenticate(password)
+        user
+    end      
 end
+  
